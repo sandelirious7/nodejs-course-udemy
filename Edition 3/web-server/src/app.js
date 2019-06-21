64ef631ab495618b71e89062acdb1ab764a286b8
Sandeep Putla <sputla@webmd.net>
2019-06-21 11:17:33 +0530

    + templating using handlebars

--- Edition 3/web-server/src/app.js
+++ Edition 3/web-server/src/app.js
@@ -1,21 +1,49 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
+// Setup handlebars engine and views directory
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup directory for static files
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sandeep Putla'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sandeep Putla'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Sandeep Putla',
+        message: 'Please contact sandeep.putla7@gmail.com!'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your Weather')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 
